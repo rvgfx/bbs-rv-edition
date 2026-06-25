c64b15eb3a526351eba483225925d6b70076e259
Raivi <ereebr1212@gmail.com>
2026-06-25 19:32:48 -0300

    Re-Added timeline markers icon

--- src/main/java/mchorse/bbs_mod/ui/utils/icons/Icons.java
+++ src/main/java/mchorse/bbs_mod/ui/utils/icons/Icons.java
@@ -234,7 +234,7 @@ public class Icons
     public static final Icon CHECKBOARD = register(new Icon(ATLAS, "checkboard", 0, 240));
     public static final Icon DISABLED = register(new Icon(ATLAS, "disabled", 16, 240));
     public static final Icon CURSOR = register(new Icon(ATLAS, "cursor", 32, 240));
-    public static final Icon TIMELINE_MARKER = register(new Icon(ATLAS,"timeline_marker",80, 160));
+    public static final Icon TIMELINE_MARKER = register(new Icon(ATLAS,"timeline_marker",96, 160));
 
     public static final Icon MOUSE_BODY = new Icon(ATLAS, "mouse_body", 241, 237, 14, 18);
     public static final Icon MOUSE_LMB = new Icon(ATLAS, "mouse_lmb", 242, 229, 6, 7);
